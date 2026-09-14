f19ba011d4d041ad1896bd6b163a6f2f6b976e9e
Creitin Gameplays <clton999@outlook.com>
2026-09-14 09:36:07 -0300

    duplicate decl

--- include/conversor.h
+++ include/conversor.h
@@ -9,7 +9,6 @@
 double power_of(int x, int y);
 int encode_utf8(uint64_t input, unsigned char utf8[4]);
 int encode_utf8_math_edition(uint64_t input, unsigned char utf8[4]);
-double power_of(int x, int y);
 
 // use unsigned char[5] to properly use encode_utf8 functions
 
